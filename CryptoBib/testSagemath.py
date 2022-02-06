6372a5c9f9efc54f089307671daa126096aeded1
Alex Jurbert <alex.jurbert15@gmail.com>
2022-02-06 16:11:22 -0500

    Création d'un script utilisant SageMath #1
    Utilisation de fonctions SageMath qui seront susceptible d'être utilisées lors du projet

--- CryptoBib/testSagemath.py
+++ CryptoBib/testSagemath.py
@@ -1,6 +1,12 @@
 from sage.all import *
 
-def testFactorial(n) :
-    return sage.all.factorial(n)
+factorielTest = sage.all.factorial(9)
 
-print(testFactorial(9))
+matriceTestA = sage.all.matrix([[1, 2, 3], [3, 2, 1], [5, 0, 2]])
+matriceTestB = sage.all.matrix([[5, 6, 1], [0, 0, 4], [3, 4, 2]])
+
+moduloTest = sage.all.mod(30, 5)
+
+print(factorielTest)
+print(matriceTestA * matriceTestB)
+print(moduloTest)
