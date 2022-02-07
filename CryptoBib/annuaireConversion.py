cb5751c5a01134f26d3e0ac0fe910629ea60e16d
Alex Jurbert <alex.jurbert15@gmail.com>
2022-02-07 10:07:28 -0500

    Ajout conversion ASCII code (int) vers char, bin, hexa #6

--- CryptoBib/annuaireConversion.py
+++ CryptoBib/annuaireConversion.py
@@ -21,3 +21,26 @@ for lettre in range(len(helloworldChar)) :
 print(helloworldCharToHex)
 #===================================#
 
+
+print("")
+helloworldASCII = helloworldCharToASCII
+#===========DEPART ASCII============#
+
+#Conversion d'une liste d'int en leur correspondance ASCII (char)
+helloworldASCIIToChar = []
+for integer in range(len(helloworldASCII)) :
+    helloworldASCIIToChar.append(chr(helloworldASCII[integer]))
+print(helloworldASCIIToChar)
+
+#Conversion d'une liste d'int en bianaire
+helloworldASCIIToBinary = []
+for integer in range(len(helloworldASCII)) :
+    helloworldASCIIToBinary.append(format(helloworldASCII[integer], 'b'))
+print(helloworldASCIIToBinary)
+
+#Conversion d'une liste d'int en hexadécimal
+helloworldASCIIToHex = []
+for integer in range(len(helloworldASCII)) :
+    helloworldASCIIToHex.append(format(helloworldASCII[integer], 'x'))
+print(helloworldASCIIToHex)
+#===================================#
