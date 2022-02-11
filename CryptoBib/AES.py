40b31b75be30c555002ac7f7d02d2fd72c655ad8
Alex Jurbert <alex.jurbert15@gmail.com>
2022-02-11 15:38:58 -0500

    Algo de hachage en 128 bits de la clé et du message fonctionnel #7

--- CryptoBib/AES.py
+++ CryptoBib/AES.py
@@ -1,29 +1,58 @@
-from operator import index
+from pickle import NONE
 from sage.all import *
-from annuaireConversion import conversionCharTo
+import annuaireConversion as AC
 
 class cleAES :
     def __init__(self, cle) :
-        hexCle = conversionCharTo(cle, 'h')
-        print(hexCle)
+        asciiCle = AC.conversionCharTo(cle, 'a')
+        print(asciiCle)
         matriceCle = sage.all.matrix(4, 4)
         indexStrCle = 0
         for i in range(4) :
             for j in range(4) :
-                matriceCle[i, j] = hexCle[indexStrCle]
+                matriceCle[i, j] = asciiCle[indexStrCle]
                 indexStrCle += 1
         self.cle = matriceCle 
             
 class messageAES :
-    def __init__(self, message) :
+    def __init__(self, message) :        
         self.messageHacher = []
-        messageHex = []
-        for char in range(len(message)) : 
-            charHex = format(ord(message[char]), "x")
-            messageHex.append(charHex)
-            
+        asciiMessage = AC.conversionCharTo(message, 'a')
+        indexStrMessage = 0
+        for parts in range(len(asciiMessage) // 16) :
+            matricePartieMessage = sage.all.matrix(4, 4)
+            for i in range(4) :
+                for j in range(4) :
+                    matricePartieMessage[i, j] = asciiMessage[indexStrMessage]
+                    indexStrMessage += 1
+            self.messageHacher.append(matricePartieMessage)    
+        if(len(asciiMessage) % 16 != 0) :   
+            matricePartieMessage = sage.all.matrix(4, 4)
+            for i in range(4) :
+                for j in range(4) :
+                    if(indexStrMessage >= len(asciiMessage)) :
+                        matricePartieMessage[i, j] = 32
+                    else :
+                        matricePartieMessage[i, j] = asciiMessage[indexStrMessage]
+                    indexStrMessage += 1
+            self.messageHacher.append(matricePartieMessage)
+        
+'''          
 mdp = "Cdfp56qpr8Z4G73c"
 cle = cleAES(mdp)
 print(cle.cle)
+'''  
+message = "Les chaussettes de l'archiduchesse sont-elle sèches, archisèches ?"
+lastBlock = 1
+if(len(message) % 16 != 0) :
+    lastBlock = 1
+else :
+    lastBlock = 0
+print("le message est d'une longueur de ", len(message), 
+      " caractères, il possède ", len(message) // 16, 
+      " blocks de 16 caractères et possède ", lastBlock, 
+      " block incomplet de ", len(message) % 16, "caractères")
+messageHacher = messageAES(message)
+print(messageHacher.messageHacher)
         
             
--- CryptoBib/testSagemath.py
+++ CryptoBib/testSagemath.py
@@ -1,12 +1,23 @@
-from sage.all import *
+from sage.all import * 
 
 factorielTest = sage.all.factorial(9)
 
-matriceTestA = sage.all.matrix([[1, 2, 3], [3, 2, 1], [5, 0, 2]])
-matriceTestB = sage.all.matrix([[5, 6, 1], [0, 0, 4], [3, 4, 2]])
+matriceTestA = sage.all.matrix(4, 4)
+for x in range(4) :
+    for y in range(4) :
+        matriceTestA[x, y] = hex(x + y)
+matriceTestB = sage.all.matrix([[5, 6, 1], [0, 0, 4], [3, 4, 2], [0, 1, 5]])
+
+hexConvert = 89
 
 moduloTest = sage.all.mod(30, 5)
+a = 10
+b = 2
+c = a // b
 
 print(factorielTest)
-print(matriceTestA * matriceTestB)
+print(matriceTestA)
+
+#print(matriceTestA * matriceTestB)
 print(moduloTest)
+print(c)
